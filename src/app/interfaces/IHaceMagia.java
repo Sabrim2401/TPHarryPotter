634c6ef77c86ad787ea7841cb3389e38c3a22b22
Sabrina Meli <sabrina.jmeli@gmail.com>
2021-04-13 19:20:00 -0300

    Se corrigio una interface, aun falta por completar.

--- src/app/interfaces/IHaceMagia.java
+++ src/app/interfaces/IHaceMagia.java
@@ -1,6 +1,15 @@
 package app.interfaces;
 
+import app.poderes.Poder;
+
 public interface IHaceMagia {
     
+    int getEnergiaMagica();
 
+    void setEnergiaMagica(int energiaMagica);
+
+    int getPoderInicial();
+
+    void setPoder(Poder poder);
+    
 }
