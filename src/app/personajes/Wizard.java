6acf1b34811dad94a3fe36dd1804d1908530a814
m.avilano@gmail.com <m.avilano@gmail.com>
2021-04-12 23:19:17 -0300

    Se agregan los atributos a las clases Personaje y Wizard. También se agregan los setters y getters de Personaje.

--- src/app/personajes/Wizard.java
+++ src/app/personajes/Wizard.java
@@ -1,5 +1,16 @@
 package app.personajes;
 
-public class Wizard extends Persona {
+public class Wizard extends Persona implements IHaceMagia {
+
+
+    private int energiaMagica;
+    private List<Hechizo> hechizos = new ArrayList<>();
+    private Escoba escoba;
+    private Poder poderInicial;
+    private Artefacto artefacto;
+    private boolean magoOscuro;
+
+
+    
     
 }
