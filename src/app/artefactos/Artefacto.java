2bb91154a1c2233b055e15970d031b02517d1539
carolinaportillo <carolinaa.portillo@hotmail.com>
2021-04-13 20:14:57 -0300

    Agrego getters y setters a la clase Artefacto.java, y un atributo provisorio para calcular el daño con amplificador.

--- src/app/artefactos/Artefacto.java
+++ src/app/artefactos/Artefacto.java
@@ -1,10 +1,47 @@
 package app.artefactos;
+
 import app.poderes.Poder;
 
 public class Artefacto {
-    public String nombre;
-    public double amplificadorDeDanio;
-    public double amplificadorDeCuracion;
-    public Poder poder;
+    private String nombre;
+    private double amplificadorDeDanio;
+    private double amplificadorDeCuracion;
+    private Poder poder;
+    private double dañoInicial;
+
+    // Agrego getters y setters de los atributos
+    // de artefacto
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getAmplificadorDeDanio() {
+        return this.amplificadorDeDanio;
+    }
+
+    public void setAmplificadorDeDanio(double amplificadorDeDanio) {
+        this.amplificadorDeDanio = amplificadorDeDanio;
+    }
+
+    public double getAmplificadorDeCuracion() {
+        return this.amplificadorDeCuracion;
+    }
+
+    public void setAmplificadorDeCuracion(double amplificadorDeCuracion) {
+        this.amplificadorDeCuracion = amplificadorDeCuracion;
+    }
+
+    public Poder getPoder() {
+        return this.poder;
+    }
+
+    public void setPoder(Poder poder) {
+        this.poder = poder;
+    }
 
 }
