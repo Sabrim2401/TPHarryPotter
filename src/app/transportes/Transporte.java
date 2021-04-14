f996ce6fee36e9729971e470f8df1f4c5d5107a6
lulinieva <evelynieva@hotmail.com>
2021-04-14 13:28:28 -0300

    Agrego el import a tranporte, genero getter y setter en Transporte.

--- src/app/transportes/Transporte.java
+++ src/app/transportes/Transporte.java
@@ -1,7 +1,35 @@
 package app.transportes;
 
-public class Transporte {
-    public String nombre;
-    public int duracionDelRecorrido;
+import app.Tansporte.transportes;
+public class Transporte  {
+
+    public Transporte(int duracionDelRecorrido, String nombre){
+
+        this.nombre = nombre;
+        this.duracionDelRecorrdio = duracionDelRecorrido;
+    }
+
+    private String nombre;
+    private int duracionDelRecorrido;
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public int getDuracionDelrecorrido(){
+        return this.duracinDelRecorrido;
+    }
+
+    public void setDuraciondelRecorrdio(int duracionDelRecorrido){
+        this.duracionDelRecorrdo = duracionDelRecorrdio;
+
+    }
+
+
+     
 
 }
