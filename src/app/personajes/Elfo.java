922dce024dadcacc4e3b047632fe52f30a6831f7
SabriM2401 <sabrina.jmeli@gmail.com>
2021-04-15 12:20:33 -0300

    Se hacen anotaciones para que brenda genere get y set en la clase Elfo.

--- src/app/personajes/Elfo.java
+++ src/app/personajes/Elfo.java
@@ -30,4 +30,23 @@ public class Elfo extends Criatura implements IHaceMagia {
       this.poderInicial = poderInicial;
    }
 
+   
+
+
+/*    abstract int getEnergiaMagica(); HECHO
+
+    abstract void setEnergiaMagica(int energiaMagica); HECHO
+
+    abstract int getPoderInicial(); HECHO
+
+    abstract void setPoder(Poder poder);
+
+    abstract Artefacto getArtefacto();
+
+    abstract void aprender(Hechizo h);
+
+    abstract void atacar(Personaje personaje, Hechizo hechizo);
+
+    abstract void atacar(Personaje personaje, String hechizo);  */
+
 }
