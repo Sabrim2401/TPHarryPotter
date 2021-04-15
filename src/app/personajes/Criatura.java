8327cc35a9b32ae0c0926209369d587a2176549c
SabriM2401 <sabrina.jmeli@gmail.com>
2021-04-14 23:22:28 -0300

    Se genera get y set de criatura haciendo el implements con IEsMagico

--- src/app/personajes/Criatura.java
+++ src/app/personajes/Criatura.java
@@ -1,5 +1,41 @@
 package app.personajes;
 
-public class Criatura extends Personaje{
+import app.interfaces.IEsMagico;
+import app.poderes.Poder;
+
+public class Criatura extends Personaje implements IEsMagico{
+
+    private int energiaMagica;
+    private Object Poder;
+
+
+    @Override
+    public int getEnergiaMagica() {
+        return this.energiaMagica;
+    }
+
+    @Override
+    public void setEnergiaMagica(int energiaMagica) {
+        this.energiaMagica = energiaMagica;
+    }
+
+    /* @Override
+     public int getPoderInicial(); */
+
+
+    @Override
+    public void setPoder(Poder poder) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getPoderInicial() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
     
+     
+  
 }
