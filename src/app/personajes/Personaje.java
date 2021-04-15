79d0839721b4f4f9e6e0e6915738ca3cc7f7da92
SabriM2401 <sabrina.jmeli@gmail.com>
2021-04-15 12:53:25 -0300

    Se arregla metodo estaVivo en Personaje.

--- src/app/personajes/Personaje.java
+++ src/app/personajes/Personaje.java
@@ -2,69 +2,67 @@ package app.personajes;
 
 public abstract class Personaje {
 
-
     private String nombre;
-    private int salud;//max 100
+    private int salud;// max 100
     private int edad;
     private int golpe; // fuerza con la que golpea
     private String color; // color del personaje para diferenciarlo
 
-
     // Método. Habría que armarlo.
 
-     // Getters y Setters de los atributos
+    // Getters y Setters de los atributos
 
-    //Nombre
-    public String getNombre(){
+    // Nombre
+    public String getNombre() {
         return this.nombre;
     }
 
-    public void setNombre(String nombre){
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     // Salud
-    public int getSalud(){
+    public int getSalud() {
         return this.salud;
     }
 
-    public void setSalud(int salud){
+    public void setSalud(int salud) {
         this.salud = salud;
     }
 
     // Edad
-    public int getEdad(){
+    public int getEdad() {
         return this.edad;
     }
 
-    public void setEdad(int edad){
+    public void setEdad(int edad) {
         this.edad = edad;
     }
 
-    //Golpe
-    public int getGolpe(){
+    // Golpe
+    public int getGolpe() {
         return this.golpe;
     }
 
-    public void setGolpe(int golpe){
+    public void setGolpe(int golpe) {
         this.golpe = golpe;
     }
 
-
-    //Golpe
-   public String getColor(){
-    return this.color;
+    // Golpe
+    public String getColor() {
+        return this.color;
     }
 
-    public void setColor(String color){
-    this.color = color;
+    public void setColor(String color) {
+        this.color = color;
     }
- 
- 
-    //metodo esta vivo // Tira error. Queda para arreglar (Bren)
-   public boolean estaVivo(){
-       if (personaje.salud > 0){
-           return false;
+
+    public boolean estaVivo(int salud) {
+        int estaVivo = 1;
+        if (salud > 1 && salud < 100) {
+            return true;
+        }
+        return false;
     }
 
 }
