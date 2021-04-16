187375eeb859b96d4f79174109f2477f44dfe06c
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-04-15 23:37:09 -0300

    genero constructor de personaje con atributos (nombre, salud, golpe, color)

--- src/app/personajes/Personaje.java
+++ src/app/personajes/Personaje.java
@@ -2,6 +2,15 @@ package app.personajes;
 
 public abstract class Personaje {
 
+    //CONSTRUCTOR CON ATRIBUTOS (nombre, salud, golpe, color) DE PERSONAJE
+     
+    public Personaje(String nombre, int salud, int golpe, String color){
+        this.nombre = nombre;
+        this.salud = salud;
+        this.golpe = golpe;
+        this.color = color;
+    }
+
     private String nombre;
     private int salud;// max 100
     private int edad;
@@ -11,7 +20,7 @@ public abstract class Personaje {
     // Método. Habría que armarlo.
 
     // Getters y Setters de los atributos
-
+    
     // Nombre
     public String getNombre() {
         return this.nombre;
