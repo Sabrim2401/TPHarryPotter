a3b4f98b7df79ac80dbe4386ec156366ac74ac3e
lulinieva <evelynieva@hotmail.com>
2021-04-16 18:24:55 -0300

    Agrego get y set en Varita.

--- src/app/artefactos/Varita.java
+++ src/app/artefactos/Varita.java
@@ -1,5 +1,18 @@
 package app.artefactos;
 
 public class Varita extends Artefacto {
+
+    public abstract class Varita {
+        
+        private String nombre;
+
+        public String getNombre() {
+            return this.nombre;
+        }
+
+        public void setNombre() {
+            this.nombre = nombre;
+        }
+    } 
     
 }
