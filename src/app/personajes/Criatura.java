b159076e24485210b936fb642be7e9f98a2e3fc8
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-04-17 18:09:51 -0300

    agrego getter y setter  de criatuara en energia magica y object poder

--- src/app/personajes/Criatura.java
+++ src/app/personajes/Criatura.java
@@ -16,6 +16,26 @@ public class Criatura extends Personaje implements IEsMagico {
     public boolean esInvisible() {
         return true;
     }
+   
+   // energia magica
 
+   public int getEnergiaMagica() {
+    return this.energiaMagica;
+    }
+
+   public void setEnergiaMagica(int energiaMagica) {
+    this.energiaMagica = energiaMagica;
+    }
+
+    //objeto poder
+    // energia magica
+
+   public Object getPoder() {
+    return this.Poder;
+    }
+
+   public void setPoder(Object Poder) {
+    this.Poder = Poder;
+    }
 
  }
