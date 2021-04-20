7a6fba23b617e0743b3885774b87a783b6149afb
m.avilano@gmail.com <m.avilano@gmail.com>
2021-04-20 17:13:40 -0300

    Se modifican los constructores de las clases CapaInvisibilidad y Horrocrux.

--- src/app/artefactos/CapaInvisibilidad.java
+++ src/app/artefactos/CapaInvisibilidad.java
@@ -4,8 +4,8 @@ import app.interfaces.IReliquiaMuerte;
 
 public class CapaInvisibilidad extends Artefacto implements IReliquiaMuerte {
 
-    public CapaInvisibilidad (String nombre, double amplificadorDeDanio, double amplificadorDeCuracion, Poder poder)  {
-        super(nombre, amplificadorDeDanio, amplificadorDeCuracion, poder);
+    public CapaInvisibilidad (String nombre, double amplificadorDeDanio, double amplificadorDeCuracion)  {
+        super(nombre, amplificadorDeDanio, amplificadorDeCuracion);
     }
 
 
--- src/app/artefactos/Horrocrux.java
+++ src/app/artefactos/Horrocrux.java
@@ -4,8 +4,8 @@ import app.interfaces.IReliquiaMuerte;
 
 public class Horrocrux extends Artefacto implements IReliquiaMuerte {
 
-    public Horrocrux (String nombre, double amplificadorDeDanio, double amplificadorDeCuracion, Poder poder)  {
-        super(nombre, amplificadorDeDanio, amplificadorDeCuracion, poder);
+    public Horrocrux (String nombre, double amplificadorDeDanio, double amplificadorDeCuracion)  {
+        super(nombre, amplificadorDeDanio, amplificadorDeCuracion);
     }
 
     // ¿ @Override ? consultar
