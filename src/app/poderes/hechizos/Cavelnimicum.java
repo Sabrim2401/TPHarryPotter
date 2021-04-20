ec9c1ae69094ddf4167a4c6bd174baf86813351a
SabriM2401 <sabrina.jmeli@gmail.com>
2021-04-20 07:54:40 -0300

    Se genera constructor en sub clase Cavelnimicum. Co-authored-by: Luli Nieva <evelynieva@hotmail.com>

--- src/app/poderes/hechizos/Cavelnimicum.java
+++ src/app/poderes/hechizos/Cavelnimicum.java
@@ -2,6 +2,8 @@ package app.poderes.hechizos;
 
 public class Cavelnimicum extends HechizoDefensa {
     
+    public Cavelnimicum (int nivelDanio, int nivelCuracion, int energiaMagica, int energiaUtilizada){
+        super(nivelDanio, nivelCuracion, energiaMagica, energiaUtilizada); }
 
     public void DefensaCanelnimicum () {
     
