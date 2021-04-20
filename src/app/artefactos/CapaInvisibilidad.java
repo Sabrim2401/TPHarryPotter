9a6931edb93d9b30759495b4e478516be1fef655
carolinaportillo <carolinaa.portillo@hotmail.com>
2021-04-20 18:51:22 -0300

    Se plantean 3 maneras de aplicar el metodo esReliquiaMuerte, queda sujeta a revision.

--- src/app/artefactos/CapaInvisibilidad.java
+++ src/app/artefactos/CapaInvisibilidad.java
@@ -1,16 +1,32 @@
 package app.artefactos;
+
 import app.poderes.*;
 import app.interfaces.IReliquiaMuerte;
 
 public class CapaInvisibilidad extends Artefacto implements IReliquiaMuerte {
 
-    public CapaInvisibilidad (String nombre, double amplificadorDeDanio, double amplificadorDeCuracion)  {
+    public CapaInvisibilidad(String nombre, double amplificadorDeDanio, double amplificadorDeCuracion) {
         super(nombre, amplificadorDeDanio, amplificadorDeCuracion);
     }
 
-    public boolean esReliquiaMuerte() {
-        // funcion para definir si es reliquia -- revisar
-        return true;
+    // Se plantean dos opciones de metodos para esReliquiaMuerte
+    // Revisar cual es correcta
+    @Override
+    public boolean esReliquiaMuerte(boolean esReliquia) {
+        esReliquia = true;
+        if (esReliquia == true)
+            return true;
     }
+
+    // @Override
+    // public boolean esReliquiaMuerte(String nombre) {
+    //// boolean esReliquia = true;
+    // if (getNombre().equals(Horrocrux)) {
+
+    // return false;
+    // }
+    // return true;
+
+    // }
 
 }
--- src/app/artefactos/PiedraResurreccion.java
+++ src/app/artefactos/PiedraResurreccion.java
@@ -1,15 +1,19 @@
 package app.artefactos;
+
 import app.poderes.*;
 import app.interfaces.IReliquiaMuerte;
 
 public class PiedraResurreccion extends Artefacto implements IReliquiaMuerte {
-  
-    public PiedraResurreccion (String nombre, double amplificadorDeDanio, double amplificadorDeCuracion)  {
+
+    public PiedraResurreccion(String nombre, double amplificadorDeDanio, double amplificadorDeCuracion) {
         super(nombre, amplificadorDeDanio, amplificadorDeCuracion);
+
     }
 
-    public boolean esReliquiaMuerte() {
-        // generar metodo para definir si es reliquia
+    @Override
+    public boolean esReliquiaMuerte(int esReliquia) {
+        if (esReliquia == 1) {
+        }
         return true;
     }
 
